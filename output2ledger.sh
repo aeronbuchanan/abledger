7486aa8e86e49c33f7a87e6a85ddd94aca622252
Aeron Buchanan <user@example.net>
2017-02-08 15:58:15 +1300

    Corrected env shell, switched to sed

--- output2ledger.sh
+++ output2ledger.sh
@@ -1,10 +1,13 @@
-#!/usr/bin.bash
+#!/bin/bash
 #
 # Concatenate all the separate account ledger files and order the entries
 # into a single ledger file
 
-echo WARNING: check creation date of output/*.csv 
+echo "WARNING: check creation date of output/*.csv"
+rm output/cat.csv output/cat_sorted.csv
 cat output/*.csv > output/cat.csv
-sort --field-separator="," -k 1,1 -k 2,2 -k 5,5 cat.csv > cat_sorted.csv
-
+sort --field-separator="," -k 1,1 -k 2,2 -k 5,5 output/cat.csv > output/cat_sorted.med
+echo "Date, Id, Account, Base, Value, Currency, Amount, Chargeable, Profit" > output/cat_sorted.csv
+sed s/,=sum.*$// < output/cat_sorted.med >> output/cat_sorted.csv
+rm output/cat_sorted.med
 
